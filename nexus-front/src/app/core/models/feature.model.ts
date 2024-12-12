e19f77ee6356f98fc362db26327f091662d94ea0
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2024-12-12 11:23:46 -0300

    Update feature.model.ts

--- nexus-front/src/app/core/models/feature.model.ts
+++ nexus-front/src/app/core/models/feature.model.ts
@@ -1,8 +1,13 @@
+// feature.model.ts
 export interface Feature {
-  id: string;
-  title: string;
-  description: string;
-  capabilities: string[];
-  icon: string;
-  benefits: string[];
+    id: string;
+    title: string;
+    description: string;
+    category?: string; // Opcional
+    capabilities?: string[]; // Opcional
+    supportedPlatforms?: string[]; // Opcional
+    icon?: string; // Opcional
+    benefits?: string[]; // Opcional
+    details?: any; // Opcional - Você pode criar uma interface para FeatureDetails se necessário
+    configuration?: any; // Opcional
 }
